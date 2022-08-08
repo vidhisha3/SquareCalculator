215fd2a210a91e3ec99e3f498d2db2c56acfdb9c
vidhisha3 <vidhishapatel@yahoo.com>
2022-08-08 18:14:10 +0100

    square calculator

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    double width = 13.5;
+    double height = 25.3;
+    double perimeter;
+    double area = width * height;
+
+//calculating the perimeter
+    System.out.println(perimeter = 2*(width + height));
+
+//calculating the Area of Rectangle
+    System.out.println("Area of Rectangle = " + area);
+    }
+}
